f94aff54e37c327ff4afcaba336fe15e7d5b62b2
LeBronLiHD <lebronlihd@outlook.com>
2021-12-04 14:58:24 +0800

    final recording vversion

--- parameters.py
+++ parameters.py
@@ -5,19 +5,19 @@ MAX_VEL_ACC = 3000
 MAX_W = 5  # rad/s
 MAX_W_ACC = 5  # rad/s^2
 
-FRESH_CYCLE = 0.2
+FRESH_CYCLE = 0.1
 
-P_W = 2
+P_W = 2.5
 I_W = 0
 D_W = 0
 
 P_V_W = 1
 
-P_V = 0.5
+P_V = 0.75
 I_V = 0
 D_V = 0
 
-DETECT_RADIUS = 350
+DETECT_RADIUS = 375
 
 LEFT_X = -3500
 LEFT_Y = -2000
